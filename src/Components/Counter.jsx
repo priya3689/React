caa400bc23ca4bd554cc262c07a6bfa4cb590ae8
sameernshamsudheen <sameernshamsudheen@gmail.com>
2024-11-25 18:15:10 +0530

    feature/router error fixed

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
-function Counter(){
-   const [count, setCount] = useState(0)
-   const handleChange =()=>{
-    if(count>0){setCount(count+1);}
-      else{setCount(0)}
-
-   }
-return <div>
-   <h1>Counter</h1>
-   <button onClick={() => handleChange()}>Add</button>
-    <div>{count}</div>
-    <button onClick={handleChange()}>Reduce</button>
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleChange = () => {
+    if (count > 0) {
+      setCount(count + 1);
+    } else {
+      setCount(0);
+    }
+  };
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button onClick={() => handleChange()}>Add</button>
+      <div>{count}</div>
+      <button onClick={() => handleChange()}>Reduce</button>
     </div>
-};
+  );
+}
 
 export default Counter;
-
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,8 +3,14 @@ export const Home = () => {
   const navigate = useNavigate();
   return (
     <div className="flex  text-3xl flex-row  justify-between   border border-red-500">
-      <div onClick={() => navigate("/todo")}> todo</div>
-      <div onClick={() => navigate("/counter")}> Counter</div>
+      <div className="cursor-pointer" onClick={() => navigate("/todo")}>
+        {" "}
+        todo
+      </div>
+      <div className="cursor-pointer" onClick={() => navigate("/counter")}>
+        {" "}
+        Counter
+      </div>
     </div>
   );
 };
